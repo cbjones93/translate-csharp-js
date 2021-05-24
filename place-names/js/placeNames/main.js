1e902159a7b126bd3b9c682f2ce4e8ec724a06e2
Cody_Jones <cbjones93@gmail.com>
2021-05-24 11:40:05 -0500

    places finished

--- place-names/js/placeNames/main.js
+++ place-names/js/placeNames/main.js
@@ -1,1 +1,18 @@
 // Put your code here
+
+const names = [
+    "Nashville", "Hong Kong", "The back yard", "Earth",
+    "London", "The mall", "Ryman Auditorium", "The Great Wall of China"
+]
+
+for (tacoName of names)
+console.log(tacoName)
+
+console.log("")
+
+const namesContainingThe = names.filter(theWords =>
+    theWords.startsWith("The"))
+
+
+for (tacoTheName of namesContainingThe)
+console.log(tacoTheName)
